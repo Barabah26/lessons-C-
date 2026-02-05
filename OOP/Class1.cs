4667651f23e5dd9174b90e85939ce3dd7120d000
pavlobarabah <pavlobarabah@gmail.com>
2026-02-05 17:40:31 +0200

    Constructors overloading. Calling one constructor from another

--- OOP/Class1.cs
+++ OOP/Class1.cs
@@ -30,6 +30,17 @@ namespace OOP
             _date = date;
         }
 
+        public MedicalAppointment(string patientName) :
+            this(patientName, 7)
+        { 
+        }
+
+        public MedicalAppointment(string patientName, int daysFromNow)
+        {
+            _patientName = patientName;
+            _date = DateTime.Now.AddDays(daysFromNow);
+        }
+
         public void Reschedule(DateTime date)
         {
             _date = date;
