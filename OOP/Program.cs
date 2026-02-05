dac7a34b58bdd2787ed2795162b9fc6994986e81
pavlobarabah <pavlobarabah@gmail.com>
2026-02-05 14:57:31 +0200

    OOP lessons

---
+++ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿var rectangle1 = new Rectangle(5, 10);
+
+Console.WriteLine(rectangle1.width);
+Console.WriteLine(rectangle1.height);
+
+
+Console.ReadKey();
+
+class Rectangle
+{
+    int width;
+    int height;
+
+}
